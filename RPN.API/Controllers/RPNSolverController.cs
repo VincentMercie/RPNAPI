7af580654c09355f18ecce2eb1b867935f77a040
Vincent <Vincentmercie@gmail.com>
2024-04-20 10:48:05 +0200

    Rajout de commentaire sur l'Endpoint et nettoyage de la chaîne passée en paramètres.

--- RPN.API/Controllers/RPNSolverController.cs
+++ RPN.API/Controllers/RPNSolverController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Console;
 using RPN.BLL.Interfaces;
 using RPN.BLL.Services;
+using System.Text.RegularExpressions;
 
 namespace RPNAPI.Controllers
 {
@@ -19,17 +21,31 @@ namespace RPNAPI.Controllers
             _rpnCalculator = rpnCalculator;
             _logger = logger;
         }
-       
+
+        /// <summary>
+        /// Endpoint permettant de résoudre une expression au format RPN (Notation Polonaise Inversée) et de renvoyer le résultat du calcul.
+        /// </summary>
+        /// <remarks>
+        /// Ce endpoint prend une expression RPN en entrée dans l'URL et tente de la résoudre. En cas de succès, le résultat du calcul est renvoyé. En cas d'erreur,
+        /// des réponses appropriées sont renvoyées pour indiquer le problème rencontré.
+        /// </remarks>
+        /// <param name="expression">Expression RPN à évaluer.</param>
+        /// <response code="200">Renvoyé si l'expression est évaluée avec succès. Le résultat du calcul est renvoyé dans le corps de la réponse.</response>
+        /// <response code="400">Renvoyé si l'expression n'est pas au format RPN valide.</response>
+        /// <response code="500">Renvoyé en cas d'erreur interne du serveur.</response>
         [HttpPost]
         [Route("{expression}")]
         public IActionResult Post(string expression)
         {
             try
             {
+                //Simple nettoyage de l'expression passée en paramètre.
+                expression = Regex.Replace(expression.Replace('.', ','), @"\s+", " ");
+
                 double result;
                 if (_inputValidator.IsValidRPN(expression))
                 {
-                     result = _rpnCalculator.RPNSolver(expression);
+                    result = _rpnCalculator.RPNSolver(expression);
                     _logger.LogInformation("Expression {expression} evaluated successfully.", expression);
                 }
                 else
@@ -37,7 +53,7 @@ namespace RPNAPI.Controllers
                     _logger.LogWarning($@"Invalid RPN expression: {expression}");
                     return BadRequest($@"le format de l'expression {expression} ne correspond pas à une expression en notation polonaise inversée");
                 }
-                 return Ok(result.ToString());
+                return Ok(result.ToString());
             }
             catch (Exception ex)
             {
